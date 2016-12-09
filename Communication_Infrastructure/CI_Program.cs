b00499b663b5c71c97778922da9beacf03de0ad2
Rodrigo Alves <rodrigo.s.alves97@gmail.com>
2016-12-09 15:38:14 +0000

    init 2

---
+++ Communication_Infrastructure/CI_Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Xml;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace Communication_Infrastructure
+{
+    class CI_Program
+    {
+        public static String DU_NODE_NAME = "SENSOR_NODE";
+        public static String ALARM_NODE_NAME = "ALARM_NODE";
+        public static String LOGGER_NODE_NAME = "LOGGER_NODE";
+
+        private static string[] m_strTopicsInfo = {DU_NODE_NAME, ALARM_NODE_NAME, LOGGER_NODE_NAME};
+        private static MqttClient m_cClient = new MqttClient(IPAddress.Parse("127.0.0.1"));
+        private static byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE };
+
+
+        static void Main(string[] args)
+        {
+            m_cClient.Connect(Guid.NewGuid().ToString());
+            if (!m_cClient.IsConnected)
+            {
+                Console.WriteLine("Error connecting to message broker...");
+                return;
+            }
+            //SPECIFY THE EVENTS WE ARE INSTERESTED ON
+            //NEW MESSAGE RECIEVED
+            m_cClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            //SUBSCRITE TO TOPICS
+            m_cClient.Subscribe(m_strTopicsInfo, qosLevels);
+
+            //FINISH THE PROGRAM
+            Console.ReadKey();
+            if (m_cClient.IsConnected)
+            {
+                m_cClient.Unsubscribe(m_strTopicsInfo); //Put this in a button to see notif!
+                m_cClient.Disconnect(); //Free process and process's resources
+            }
+        }
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+
+            //RECIEVED A MESSAGE
+            Console.WriteLine("Received = " + Encoding.UTF8.GetString(e.Message) + " on topic " + e.Topic + "\n");
+
+            if (e.Topic == DU_NODE_NAME)
+            {
+                messageReceived(e.Message);
+            }
+        }
+
+        private static void messageReceived(byte[] message)
+        {
+            XmlDocument doc = new XmlDocument();
+            String xml = Encoding.UTF8.GetString(message);
+            doc.LoadXml(xml);
+            Console.WriteLine(doc.OuterXml);
+            XmlNode nome = doc.SelectSingleNode("/sensor/name");
+        }
+       
+    }
+}
---
+++ SmartH2O_DU/DU_Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Xml;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace SmartH2O_DU
+{
+    class DU_Program
+    {   
+        public static String DU_NODE_NAME = "SENSOR_NODE";
+        public static MqttClient m_cClient = new MqttClient(IPAddress.Parse("127.0.0.1"));
+        public
+        
+        static void Main(string[] args)
+        {
+            DU_Program du_Program = new DU_Program();
+            
+            SensorNodeDll.SensorNodeDll dll = new SensorNodeDll.SensorNodeDll();
+            dll.Initialize(GeneratedMessage, 1000);
+            
+        }
+
+        public DU_Program() {
+            m_cClient.Connect(Guid.NewGuid().ToString());
+        }
+
+        public static void GeneratedMessage(string str)
+        {
+            sendMessage(createXMLDoc(str));
+        }
+
+        public static String createXMLDoc(string message)
+        {
+            String[] messages = message.Split(';');
+            XmlDocument doc = new XmlDocument();
+            XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(docNode);
+            XmlNode sensorNode = doc.CreateElement("SensorNode");
+            doc.AppendChild(sensorNode);
+
+            //numero de indice         //moleculas            //ph              //data(para as estatisticas)
+            XmlElement number = doc.CreateElement("id");
+            number.InnerText = messages[0];
+
+            XmlElement molecule = doc.CreateElement("molecule");
+            molecule.InnerText = messages[1];
+
+            XmlElement ph = doc.CreateElement("value");
+            ph.InnerText = messages[2];
+
+            XmlElement data = doc.CreateElement("Data");
+            data.InnerText = DateTime.Now.ToString("dd/mm/yyyy hh:mm:ss");
+
+            sensorNode.AppendChild(number);
+            sensorNode.AppendChild(molecule);
+            sensorNode.AppendChild(ph);
+            sensorNode.AppendChild(data);
+
+            return doc.OuterXml;
+        }
+
+        public static void sendMessage(String XMLMessage)
+        {
+            if (!m_cClient.IsConnected)
+            {
+                Console.WriteLine("Error connecting to message broker...");
+                return;
+            }
+            Console.Write("Sending Message:" + XMLMessage + "\n");
+            m_cClient.Publish(DU_NODE_NAME, Encoding.UTF8.GetBytes(XMLMessage));
+        }
+    }
+}
